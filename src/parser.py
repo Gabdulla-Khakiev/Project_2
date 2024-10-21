1a13dac6d2cf18c0c99fd7006629e4a6fc82c0ca
Gabdulla <gabdulla2001@gmail.com>
2024-10-21 21:30:22 +0300

    refactor parser.py

---
+++ src/parser.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+    """Абстрактный класс для работы с API сервисов вакансий"""
+
+    def __init__(self, file_worker):
+        self.file_worker = file_worker
+
+    @abstractmethod
+    def load_vacancies(self, keyword: str):
+        """
+        Метод для загрузки вакансий по ключевому слову.
+        :param keyword: Ключевое слово для поиска вакансий
+        """
+        pass
+
+    @abstractmethod
+    def save_vacancies(self):
+        """
+        Метод для сохранения загруженных вакансий в файл.
+        """
+        pass
